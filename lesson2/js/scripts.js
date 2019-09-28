b542d1e289191b657404e2719f164bb5d3b9be95
joelwcannon <joelcannon@byui.edu>
2019-09-28 11:31:20 -0600

    basic structure

---
+++ lesson2/js/scripts.js
@@ -0,0 +1,6 @@
+var today = new Date(); 
+var year = today.getFullYear();
+
+document.getElementById('copyYear').innerHTML = year;
+
+document.getElementById('timestamp').innerHTML = document.lastModified;
