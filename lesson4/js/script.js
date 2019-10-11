f703d56bf95f56d8031a1b090f0b2b3a17692126
joelwcannon <joelcannon@byui.edu>
2019-10-11 12:28:14 -0600

    fixed js for footer

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,10 @@
+var today = new Date(); 
+var year = today.getFullYear();
+
+document.getElementById('copyYear').innerHTML = year;
+
+document.getElementById('timestamp').innerHTML = document.lastModified;
+
 function toggleMenu() {
    console.log("it worked!")
    document.getElementsByClassName("navigation")[0].classList.toggle("hide");
