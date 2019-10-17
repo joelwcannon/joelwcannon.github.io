0be4eb2bc89020c43e878c1506cf2170f68c1b3e
joelwcannon <joelcannon@byui.edu>
2019-10-17 14:08:45 -0600

    added banner w/ close icon

---
+++ lesson5/js/script.js
@@ -0,0 +1,14 @@
+const today = new Date(); 
+const localeDate = new Intl.DateTimeFormat("en-GB", {
+   weekday: "long",
+   year: "numeric",
+   month: "long",
+   day: "numeric"
+});
+
+document.getElementById('copyYear').innerHTML = today.getFullYear();
+document.getElementById('timestamp').innerHTML = localeDate.format(today);
+
+function toggleMenu() {
+   document.getElementsByClassName("navigation")[0].classList.toggle("hide");
+}
