ab3409d59a34fd10e83e4aedaf9cd00702fce566
joelwcannon <joelcannon@byui.edu>
2019-11-13 01:30:09 -0700

    cleaned up formatting

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -19,23 +19,21 @@ fetch(requestURL)
          let fullName = prophets[i].name + ' ' + prophets[i].lastname;
          let nameEl = document.createElement('h2');
          nameEl.textContent = fullName;
+         card.appendChild(nameEl);
 
          let birthEl = document.createElement('p');
          let birthDate = new Date(prophets[i].birthdate);
-
          birthEl.textContent = `Born: ${localeDate.format(birthDate)} in ${prophets[i].birthplace}`;
+         card.appendChild(birthEl);
 
          let deathEl = document.createElement('p');
          let deathDate = new Date(prophets[i].death);
          deathEl.textContent = `Died: ${localeDate.format(deathDate)}`;
+         card.appendChild(deathEl);
 
          let imageEl = document.createElement('img');        
          imageEl.setAttribute('src', prophets[i].imageurl);
          imageEl.setAttribute('alt', fullName + ' - ' + prophets[i].order);
-
-         card.appendChild(nameEl);
-         card.appendChild(birthEl);
-         card.appendChild(deathEl);
          card.appendChild(imageEl);
 
          document.querySelector('div.cards').appendChild(card);
