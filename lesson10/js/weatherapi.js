5c7df2a9e450a61222e71fbd55fd792c43ce7b46
joelwcannon <joelcannon@byui.edu>
2019-11-22 23:25:30 -0700

    using toPrecision(2) for windspeed

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -10,8 +10,9 @@ const dow = new Intl.DateTimeFormat("en-US", {
 fetch(weatherAPIURL)
    .then((response) => response.json())
    .then((jsObject) => {
+      console.log(jsObject);
       const tempF = jsObject.main.temp.toFixed(0);
-      const windSpeedMPH = jsObject.wind.speed.toFixed(0);
+      const windSpeedMPH = jsObject.wind.speed.toPrecision(2);
       const windChillF = windChill(tempF, windSpeedMPH);
       document.getElementById('temperature').textContent = tempF;
       document.getElementById('hightemp').textContent = jsObject.main.temp_max.toFixed(0);
